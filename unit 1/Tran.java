11fc3e0d8b123241a3e2c27b8c4e605b5a9e61aa
Danny Tran <114210595+dannytrn@users.noreply.github.com>
2023-07-21 00:51:47 -0400

    Add files via upload

---
+++ unit 1/Tran.java
@@ -0,0 +1,13 @@
+public class Tran{
+   public static void main(String[] args)
+   {
+int x=3, y=2, z=1;
+if ( x== y-1) {
+if (y==x+1){
+System.out.print(x+ " ");
+} else {
+System.out.print("wrong");
+}
+}
+}
+}
